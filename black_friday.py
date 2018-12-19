5abc96795f5c3429fc18c5bbda16adc9e7ae9395
ewedro <dulianka@gmail.com>
2018-12-19 21:37:04 +0100

    column analysis

--- black_friday.py
+++ black_friday.py
@@ -11,3 +11,29 @@ print(friday.shape)
 #%%
 # first 10 rows
 print(friday.head(10))
+
+#%%
+# let's look at column types
+print(friday.dtypes)
+
+#%% 
+# learn some more about purchase price
+purch = friday["Purchase"]
+print(purch.describe())
+
+#%%
+# take a look at row with purchase with highest price
+highest_purch = friday.loc[friday["Purchase"]==23961]
+print(highest_purch)
+
+#%% 
+# exploring Age column
+age = friday["Age"]
+print(age.value_counts())
+
+#%%
+# take a look at the same column but with gender split
+age_female = friday.loc[friday["Gender"]=="F","Age"]
+print(age_female.value_counts())
+age_male = friday.loc[friday["Gender"]=="M","Age"]
+print(age_male.value_counts())
